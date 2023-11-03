5ee87e6a40e400d1b94c7b90791de600c8919c76
Tommy <143513100+TommyBlanche@users.noreply.github.com>
2023-11-02 21:49:04 -0400

    f

--- Assignment 5/Debug.py
+++ Assignment 5/Debug.py
@@ -1,38 +1,56 @@
 def main():
     #good working example!
-    stringInput = input("enter a string")
+    stringInput = input("enter a string: ")
     if stringInput.isalpha():
         print("string!")
     else:
         print("not string :(")
         
     #can you google and find what function you should use to check if it's numeric (an int?)?
-    intInput = input("enter an int")
-    if intInput.?:
+    intInput = input("enter an int: ")
+    if intInput.isnumeric():
         print("int!")
     else:
         print("not int :(")
     
     #what about if it's both letters and numbers?
-    alphIntInput = input("Enter letters and numbers")
-    if alphIntInput.?:
+    alphIntInput = input("Enter letters and numbers: ")
+    if alphIntInput.isalnum():
         print("Letters and numbers!")
     else:
         print("weird characters :(")
        
     #good working example
-    asterisk = input("Enter an asterisk please")
+    asterisk = input("Enter an asterisk please: ")
     if asterisk == "*":
         print("good!")
     else:
         print("not asterisk :(")
         
     #now write code to check if the input was either an asterisk OR an ampersand (&)
+
+    character = input("Enter either an asterisk or ampersand: ")
+    if character == "*":
+        print("That is an asterick")
+    elif character == "&":
+        print("That is an ampersand")
+    else:
+        print("What the hell is that???????")
     
         
     #do the live example we did in class: ask user to input an integer, but before you cast it to an int, check that it's an integer before doing your variable = int(variable) command
-    
+
+    number = int(input("Put in an integer: "))
+
+    number = int(number)
+
+    print(number)
+
+
+
     # last challenge: find out how to check if the string input has the substring "marist"
     #google this one ;) substring is the key google term
+
+
     
 main()
